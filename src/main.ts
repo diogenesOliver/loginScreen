9096d5097290ffa13baf5c08aed027b748caa8bd
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-03-08 19:19:01 -0300

    feat: Import reflect-metadat to main file an dependency injection directory

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata'
+import './shared/index'
+
 import dotenv from 'dotenv'
 import express from 'express'
 import path from 'path'
@@ -16,28 +18,3 @@ app.use('/api', express.json(), express.urlencoded({ extended: true }), routes)
 app.use('/api', express.static(path.join(__dirname, 'public')))
 
 export { app }
-
-/* JS */
-
-/* require('dotenv').config()
-
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const apiRoute = require('./routes/api')
-
-const app = express()
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'public'))
-
-app.use('/api', express.json(), express.urlencoded({ extended: true }), apiRoute)
-app.use('/api', express.static(path.join(__dirname, 'public')))
-
-mongoose.connect(process.env.URL_DATABASE).then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Connect in DataBase - Server run on port: ${process.env.PORT}`)
-    })
-}).catch((error) => {
-    console.log(error)
-}) */
