d48129497d5a36f598903333ace150d3a4ea371e
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-03-09 09:16:06 -0300

    feat: Repository pattern to create user feature

--- src/repositories/implementation/CreateUserRepository.ts
+++ src/repositories/implementation/CreateUserRepository.ts
@@ -1,9 +1,10 @@
+import { User } from "../../domain/models/User";
 import { UserEntitie } from "../../entities/User";
 import { ICreateUserRepository } from "../ICreateUserRepository";
 
 export class CreateUserRepository implements ICreateUserRepository{
     async save(userData: UserEntitie): Promise<UserEntitie> {
-        /* Create Query */
+        User.create(userData)
         return userData
     }
 }
