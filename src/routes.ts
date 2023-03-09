ece9a0fe35f6a35498e073f4e8afc3dedf03a60b
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-03-09 09:19:40 -0300

    feat: route to create user feature

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 
+import { createUserUseCaseInstance } from "./useCases/CreateUser/createUserIndex";
+
 const routes = Router()
 
 routes.get('/home-page', (req: Request, res: Response) => {
@@ -12,6 +14,10 @@ routes.get('/login-user', (req: Request, res: Response) => {
 
 routes.get('/user-exist', (req: Request, res: Response) => {
     res.render('login')
+})
+
+routes.post('/register', (req: Request, res: Response) => {
+    return createUserUseCaseInstance.handle(req, res)
 })
 
 export { routes }
